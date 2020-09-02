e9537ddb025afade918090e446137d7d823a5db9
lanningbo <lanningbo@netposa.com>
2020-09-02 18:49:13 +0800

    没改啥

--- middleware/corsmiddleware.go
+++ middleware/corsmiddleware.go
@@ -1,13 +1,15 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"net/http"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+
+	f:= func(ctx *gin.Context) {
 		//前端请求端口是8080
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", viper.GetString("auth.Access-Control-Allow-Origin"))
 		ctx.Writer.Header().Set("Access-Control-Max-Age", viper.GetString("auth.Access-Control-Max-Age")) //过期时间
@@ -15,10 +17,13 @@ func CORSMiddleware() gin.HandlerFunc {
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", viper.GetString("auth.Access-Control-Allow-Headers"))
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", viper.GetString("auth.Access-Control-Allow-Credentials"))
 
+		fmt.Println("origin: ",viper.GetString("auth.Access-Control-Allow-Origin"))
+
 		if ctx.Request.Method == http.MethodOptions {
 			ctx.AbortWithStatus(http.StatusOK)
 		} else {
 			ctx.Next()
 		}
 	}
+	return f
 }
